3549005972a7ca92aff6109ed5f7826be2139af5
Bo <zboyang@umich.edu>
2019-10-29 19:54:45 -0400

    Added gitignore for .meta files, animations for bajesus added, meepos added

---
+++ Head_bob.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head_bob : MonoBehaviour
+{
+    public float bob_distance = 5f;
+
+    private float traveled;
+    private int dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        traveled = bob_distance / 2;
+        dir = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep going
+        if (traveled < bob_distance)
+        {
+
+        }
+        else // Reverse
+        {
+            dir *= -1;
+        }
+
+
+    }
+}
